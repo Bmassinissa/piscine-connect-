fb0064d6c5997744b701cfd16abf18703dfbe3c6
tina-creator <chioutz91@gmail.com>
2021-03-18 10:25:48 +0100

    page web

---
+++ site_web/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import LineChart from './components/LineChart'
+
+function App() {
+  return (
+    <div className="App">
+      <div className='chart'>
+        <LineChart/>
+      </div>
+
+    </div>
+  )
+}
+
+export default App;
